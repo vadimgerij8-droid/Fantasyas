c8726a3e8784bdecabb86350d233a784d0ba08b1
vadimgerij8-droid <vadimgerij8@gmail.com>
2026-03-03 15:44:18 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,20 @@
+// ================= Firebase імпорти =================
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
+
+// ================= Конфігурація =================
+const firebaseConfig = {
+  apiKey: "AIzaSyDRzC-QDE0-UXd-XL0i3iqayFiKcc6wmvc",
+  authDomain: "fantasyasapp.firebaseapp.com",
+  projectId: "fantasyasapp",
+  storageBucket: "fantasyasapp.appspot.com",
+  messagingSenderId: "721763921060",
+  appId: "1:721763921060:web:3d61044ea2424e8176ca31"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
